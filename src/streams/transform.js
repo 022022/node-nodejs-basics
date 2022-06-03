0763d638dbd33c1684880594c2f0a6d282dcfb32
Zhenya <evgeegai@gmail.com>
2022-06-03 19:21:18 +0300

    feat: streams solved

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,21 @@
+import { Transform, pipeline } from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+
+  const reverseStr = new Transform({
+    transform(chunk, encryption, callback){
+      const str = chunk.toString();
+      const strReversed = str.split('').reverse().join('');
+      this.push(strReversed);
+      callback();
+    }
+  });
+
+  pipeline(process.stdin, reverseStr, process.stdout, ()=>{});
+
 };
+
+transform();
+
+//implement function that reads data from process.stdin,
+//reverses text using Transform Stream and then writes it into process.stdout
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,16 @@
+import { fileURLToPath } from 'url'
+import { join, dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import { createWriteStream } from 'fs';
+
 export const write = async () => {
-    // Write your code here 
+  const pathToFile = join(__dirname, 'files', 'fileToWrite.txt');
+
+  const writableStream = createWriteStream(pathToFile);
+
+  process.stdin.on('data', chunk => writableStream.write(chunk));
 };
+
+write();
