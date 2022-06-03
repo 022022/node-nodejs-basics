6c3c0230699037bb6a62746522b487d2d3a58f6b
Zhenya <evgeegai@gmail.com>
2022-06-03 10:26:01 +0300

    feat: hash solved

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,22 @@
+import { fileURLToPath } from 'url'
+import { join, dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    const data = await readFile(pathToFile);
+
+    const hashObj = createHash('sha256');
+    hashObj.update(data);
+    const hex = hashObj.digest('hex');
+    
+    console.log(hex);
+    return hex;
 };
+
+calculateHash();
