9e06bb8318f84e9cb557c66e60063c4ae9d35d63
Zhenya <evgeegai@gmail.com>
2022-06-05 19:16:55 +0300

    feat: Worker Threads almost solved

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,23 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
 export const performCalculations = async () => {
-    // Write your code here
+  const basicNum = 10;
+  const numberOfCpuCores = cpus().length;
+  let resultsArr = [];
+
+  for (let i = 0; i < numberOfCpuCores; i++){
+    const worker = new Worker('./worker.js');
+    const argForWorker = basicNum + i;
+    worker.postMessage(argForWorker);
+    worker.on('message', (msg) => {
+      console.log(argForWorker +' result '+ msg.status + msg.data );
+      resultsArr.push(msg);
+      console.log(resultsArr);
+    });
+  }
+
+  console.log(resultsArr);
 };
+
+performCalculations();
